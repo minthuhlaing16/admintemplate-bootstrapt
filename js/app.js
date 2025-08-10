9ec6c7f7e1a4002661eb10f1031764132d138de9
minthuhlaing16 <minthuhlaing786@gmail.com>
2025-08-10 13:57:04 +0630

    admin project day two

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// Start Navbar
+
+// start top navbar
+function dropbtn(e) {
+  //   console.log(e.target);
+  //   console.log(e.target.parentElement);
+  //   console.log(e.target.parentElement.nextElementSibling);
+
+  e.target.parentElement.nextElementSibling.classList.toggle("show");
+}
+// end top navbar
+
+// End Navbar
+
+$(document).ready(function () {
+  // Start Navbar
+  // start left sidebar
+  $(".sidebarlinks").click(function () {
+    $(".sidebarlinks").removeClass("currents");
+    $(this).addClass("currents");
+  });
+  // end left sidebar
+  // End Navbar
+});
