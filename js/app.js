f5e1743ad3d6a3eee538b43e5d06272130b1e71e
minthuhlaing16 <minthuhlaing786@gmail.com>
2025-08-13 18:17:25 +0630

    finish admin pj

--- js/app.js
+++ js/app.js
@@ -30,6 +30,96 @@ $(document).ready(function () {
   // End Navbar
 });
 
+// Start Gauge Area
+
+var gaugeusers = new JustGage({
+  id: "gaugeusers", // the id of the html element
+  value: 50,
+  min: 0,
+  max: 100,
+  decimals: 2,
+  gaugeWidthScale: 0.6,
+});
+var gaugecustomers = new JustGage({
+  id: "gaugecustomers", // the id of the html element
+  value: 50,
+  min: 0,
+  max: 100,
+  decimals: 2,
+  gaugeWidthScale: 0.6,
+});
+var gaugeemployees = new JustGage({
+  id: "gaugeemployees", // the id of the html element
+  value: 50,
+  min: 0,
+  max: 100,
+  decimals: 2,
+  gaugeWidthScale: 0.6,
+});
+var gaugeinvestors = new JustGage({
+  id: "gaugeinvestors", // the id of the html element
+  value: 50,
+  min: 0,
+  max: 100,
+  decimals: 2,
+  gaugeWidthScale: 0.6,
+});
+
+// update the value randomly
+
+setInterval(() => {
+  gaugeusers.refresh(Math.random() * 100);
+  gaugecustomers.refresh(Math.random() * 100);
+  gaugeemployees.refresh(Math.random() * 100);
+  gaugeinvestors.refresh(Math.random() * 100);
+}, 5000);
+
+// End Gauge Area
+
+// Start Expenses Area
+const ctx = document.getElementById("myChart");
+
+new Chart(ctx, {
+  type: "doughnut",
+  data: {
+    datasets: [
+      {
+        data: [12, 19, 3],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {},
+});
+// End Expenses Area
+
+// Start Earning Area
+google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ["Year", "Sales", "Expenses"],
+    ["2004", 1000, 400],
+    ["2005", 1170, 460],
+    ["2006", 660, 1120],
+    ["2007", 1030, 540],
+  ]);
+
+  var options = {
+    title: "Sales Performance",
+    curveType: "function",
+    legend: { position: "bottom" },
+  };
+
+  var chart = new google.visualization.LineChart(
+    document.getElementById("curve_chart")
+  );
+
+  chart.draw(data, options);
+}
+// End Earning Area
+
 // Start Footer
 
 const getyear = document.getElementById("getyear");
